4ac2a590795847c33a7a30e2b6fcb91e5f1456f2
Antonio Recalde <professor.arecalde@gmail.com>
2021-02-14 23:44:00 -0800

    added comments

--- app.py
+++ app.py
@@ -7,6 +7,12 @@ def hello_world():
 
 @app.route('/hello', methods=["GET"])
 def get_hello():
+    """Demonstrates GET request and getting data from Query String
+
+       Test in Postman: Import -> Paste Raw Text:
+       curl --location --request GET 'http://127.0.0.1:5001/hello?name=James' \
+        --header 'Content-Type: application/json'
+    """
     # this is how to extract parameters from query string
     user_name = request.args.get('name')
     message = "hello {}".format(user_name)
@@ -15,6 +21,13 @@ def get_hello():
 
 @app.route('/hello', methods=["POST"])
 def post_hello():
+    """Demonstrates POST request and getting json data from client
+
+        Test in Postman: Import -> Paste Raw Text:
+        curl --location --request POST 'http://127.0.0.1:5001/hello' \
+        --header 'Content-Type: application/json' \
+        --data-raw '{ "name": "Jonathan" }'
+    """
     # this is how to get the json payload
     json_payload = request.get_json()
     print(json_payload)
