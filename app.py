d64357a3773174678538364e17d772aed3ca8246
Antonio Recalde <professor.arecalde@gmail.com>
2021-02-14 20:27:58 -0800

    getting query parameters and json payload from request

--- app.py
+++ app.py
@@ -1,15 +1,26 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return jsonify(['Jim', 'James', 'John'])
 
-@app.route('/hello', methods=["GET", "POST"])
-def hello():
-    return jsonify(['Jim', 'James', 'John'])
+@app.route('/hello', methods=["GET"])
+def get_hello():
+    # this is how to extract parameters from query string
+    user_name = request.args.get('name')
+    message = "hello {}".format(user_name)
+    print(message)
+    return jsonify({"message": message})
 
+@app.route('/hello', methods=["POST"])
+def post_hello():
+    # this is how to get the json payload
+    json_payload = request.get_json()
+    print(json_payload)
+    return jsonify({"json_payload": json_payload})
 
 @app.route('/my-website')
 def my_website():
-    return render_template('hello.html', name="Antonio")
+    user_name = request.args.get('name')
+    return render_template('hello.html', name=user_name)
